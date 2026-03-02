c7d3c0a73a7e051e6fdd429432cf47e375314231
MosarafWorld <mosarafworld.078692@gmail.com>
2026-03-02 16:45:37 +0530

    adding dirbase statics

--- mw_pro/mw_pro/settings.py
+++ mw_pro/mw_pro/settings.py
@@ -116,5 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  ## Hossain has added this, YOu can remove it once completed the project.
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+
+
+
